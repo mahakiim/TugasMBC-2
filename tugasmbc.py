27e1cecc49829630d9c054a6ab34061ba67ee870
Muhammad Hakim <139327011+mahakiim@users.noreply.github.com>
2023-07-17 22:03:18 +0700

    Update tugasmbc.py
    Telah selesai mengerjakan soal no 1

--- tugasmbc.py
+++ tugasmbc.py
@@ -12,3 +12,24 @@ Kode program kemudian menampilkan harga yang harus dibayar setelah dikurangi dis
 
 ==============================================================================='''
 
+print('## Program Python Diskon Potongan Harga ##')
+print('=========================================') 
+print() 
+
+totalbelanja = int(input('Total Belanja: Rp.'))
+
+if (totalbelanja >= 100000) and (totalbelanja < 500000):
+    harga_final = totalbelanja - (0.1*totalbelanja)
+    print('Selamat, anda dapat diskon 10%')
+elif (totalbelanja >= 500000) and (totalbelanja < 1000000):
+    harga_final = totalbelanja - (0.2*totalbelanja)
+    print('Selamat, anda dapat diskon 20%')
+elif (totalbelanja >= 1000000):
+    harga_final = totalbelanja - (0.3*totalbelanja)
+    print('Selamat, anda dapat diskon 30%')
+else:
+    harga_final = totalbelanja
+    print('Maaf, anda tidak mendapat diskon')
+
+print('Total yang harus dibayar: Rp.', harga_final)
+
